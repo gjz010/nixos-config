0d5c47a92cba4443cac5bb031acbd0f14ab9add5
gjz010 <gjz010944@gmail.com>
2023-08-19 23:32:57 +0800

    Initial commit from nixos-desktop.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs?rev=4107024ef4d9f637b568296f40a2ba0f62b13437";
+    nixpkgs.url = "path:/home/gjz010/playground/nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
