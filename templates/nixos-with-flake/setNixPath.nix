1cf6bdd1a340ac615355acc82c2c54a0f51316ec
gjz010 <gjz010944@gmail.com>
2024-03-28 23:49:01 +0800

    nixos-with-flake: enable flake by default

--- templates/nixos-with-flake/setNixPath.nix
+++ templates/nixos-with-flake/setNixPath.nix
@@ -4,5 +4,6 @@
   nix.registry.nixpkgs.flake = nixpkgs;
   nix.registry.nixos-configuration.flake = self;
   nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.configurationRevision = self.rev or "dirty";
 }
