d7b15860c76d94aa5d423321af608fe32a3a5ae5
gjz010 <gjz010944@gmail.com>
2024-04-20 19:29:36 +0800

    fix: nix-user-chroot

--- pkgs/nix-user-chroot/default.nix
+++ pkgs/nix-user-chroot/default.nix
@@ -1,26 +1,17 @@
-{ pkgsStatic, rust, rust-bin, fetchFromGitHub, lib, makeRustPlatform }:
-let
-  target = rust.toRustTarget pkgsStatic.stdenv.targetPlatform;
-  rustStatic = rust-bin.stable.latest.minimal.override {
-    targets = [ target ];
-  };
-  rustPlatform = pkgsStatic.makeRustPlatform {
-    cargo = rustStatic;
-    rustc = rustStatic;
-  };
+{ pkgsStatic, fetchFromGitHub }:
+pkgsStatic.rustPlatform.buildRustPackage rec {
+  pname = "nix-user-chroot";
+  version = "1.2.2";
   src = fetchFromGitHub {
     owner = "nix-community";
     repo = "nix-user-chroot";
     sha256 = "8w2/Ncfcg6mMRFgMZg3CBBtAO/FI6G6hDMyaLCS3hwk=";
     rev = "1.2.2";
   };
-in
-rustPlatform.buildRustPackage {
-  pname = "nix-user-chroot";
-  version = "1.2.2";
-  inherit src;
+
   cargoLock = {
-    lockFile = ./Cargo.lock;
+    lockFile = "${src}/Cargo.lock"; # IFD here
   };
   doCheck = false;
 }
+  
