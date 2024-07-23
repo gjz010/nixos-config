52a5b7c498496c449b1d1adfdd8dae4e67b04e87
gjz010 <gjz010944@gmail.com>
2024-07-23 19:39:30 +0800

    module: auth-thu

--- modules/auth-thu.nix
+++ modules/auth-thu.nix
@@ -1,51 +1,59 @@
 flake@{ inputs, self }:
-{config, lib, pkgs, ...}:{
-  options.services.auth-thu = {
-    enable = lib.mkEnableOption "Enable auth-thu.";
-    configPath = lib.mkOption {
+{ config, lib, pkgs, ... }:
+let cfg = config.services.auth-thu;
+in
+{
+  options = {
+    services.auth-thu = {
+      enable = lib.mkEnableOption "Enable auth-thu.";
+      configPath = lib.mkOption {
         type = lib.types.str;
         description = "Configuration for auth-thu.";
-    };
-    package = lib.mkOption {
-        default = inputs.gjz010.packages."config.nixpkgs.system".auth-thu;
+      };
+      package = lib.mkOption {
+        default = inputs.gjz010.packages."${config.nixpkgs.system}".goauthing;
         description = "Specify which GoAuthing to use";
         type = lib.types.package;
+      };
     };
-  };
-  options.gjz010.secrets.auth-thu = {
-    enable = lib.mkEnableOption ''
+    gjz010.secrets.auth-thu = {
+      enable = lib.mkEnableOption ''
         Use repository-included configuration for auth-thu.
         You should not enable this if you are not me.
-    '';
+      '';
+    };
   };
 
-  config = (lib.mkIf config.services.auth-thu.enable {
-      systemd.services.auth-thu = 
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      systemd.services.auth-thu =
         let
-            auth-thu = services.auth-thu.package;
-            thuconfig = services.auth-thu.configPath;
+          auth-thu = cfg.package;
+          thuconfig = cfg.configPath;
         in
         {
-            enable = true;
-            description = "auth-thu from GoAuthing.";
-            unitConfig = {
+          enable = true;
+          description = "auth-thu from GoAuthing.";
+          unitConfig = {
             After = [ "network.target" "nss-lookup.target" ];
-            };
-            serviceConfig = {
+          };
+          serviceConfig = {
             ExecStartPre = [
-                "-${auth-thu}/bin/auth-thu -c ${thuconfig} -D auth"
-                "-${auth-thu}/bin/auth-thu -c ${thuconfig} -D login"
+              "-${auth-thu}/bin/auth-thu -c ${thuconfig} -D auth"
+              "-${auth-thu}/bin/auth-thu -c ${thuconfig} -D login"
             ];
-            ExecStart =  "${auth-thu}/bin/auth-thu -c ${thuconfig} -D online";
+            ExecStart = "${auth-thu}/bin/auth-thu -c ${thuconfig} -D online";
             Restart = "always";
             RestartSec = 5;
-            };
-            wantedBy = [ "multi-user.target" ];
+          };
+          wantedBy = [ "multi-user.target" ];
         };
-  }) // (lib.mkIf config.gjz010.secrets.auth-thu.enable {
-    sops.secrets."auththu.json" = {
+    })
+    (lib.mkIf config.gjz010.secrets.auth-thu.enable {
+      sops.secrets."auththu.json" = {
         sopsFile = "${config.passthru.gjz010.secretRoot}/router/auththu.yaml";
-    };
-    services.auth-thu.configPath = config.sops.secrets."auththu.json".path;
-  });
+      };
+      services.auth-thu.configPath = config.sops.secrets."auththu.json".path;
+    })
+  ];
 }
--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,26 +1,28 @@
 flake@{ inputs, self }:
-{config, lib, pkgs, ...}:{
+{ config, lib, pkgs, ... }: {
   options.gjz010.drivers.nvk = {
     enable = lib.mkEnableOption "Enable NVK.";
   };
   options.gjz010.drivers.nvidia-proprietary = {
     enable = lib.mkEnableOption "Enable Proprietary Nvidia Driver.";
     driverPkg = lib.mkOption {
-        default = config.boot.kernelPackages.nvidiaPackages.beta;
-        description = "Nvidia proprietary driver version.";
-        type = lib.types.package;
+      default = config.boot.kernelPackages.nvidiaPackages.beta;
+      description = "Nvidia proprietary driver version.";
+      type = lib.types.package;
     };
     # https://github.com/NixOS/nixpkgs/issues/254614
     openKernelDriver = lib.mkEnableOption "Use open kernel driver.";
   };
 
-  config = (lib.mkIf config.gjz010.drivers.nvk.enable {
+  config = lib.mkMerge [
+    (lib.mkIf config.gjz010.drivers.nvk.enable {
       boot.kernelParams = [
         "nouveau.config=NvGspRM=1"
         "nouveau.debug=info,VBIOS=info,gsp=debug"
       ];
       services.xserver.videoDrivers = [ "modesetting" ];
-  }) // (lib.mkIf config.gjz010.drivers.nvidia-proprietary.enable {
+    })
+    (lib.mkIf config.gjz010.drivers.nvidia-proprietary.enable {
       services.xserver.videoDrivers = [ "nvidia" ];
       boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
       hardware.nvidia = {
@@ -33,5 +35,6 @@ flake@{ inputs, self }:
       };
       hardware.opengl.driSupport = false;
       hardware.opengl.package = config.gjz010.drivers.nvidia-proprietary.driverPkg;
-  });
+    })
+  ];
 }
