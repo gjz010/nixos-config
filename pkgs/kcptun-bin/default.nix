ef30e6e20984149f959c7d91a081605f2c03304f
gjz010 <gjz010944@gmail.com>
2024-07-23 19:40:36 +0800

    nix fmt

--- pkgs/kcptun-bin/default.nix
+++ pkgs/kcptun-bin/default.nix
@@ -1,27 +1,27 @@
-{ autoPatchelfHook, stdenvNoCC, lib, unar}:
+{ autoPatchelfHook, stdenvNoCC, lib, unar }:
 let
-inherit (builtins) fetchurl;
-kcptunBinaries = {
-  "x86_64-linux" = rec {
-    version = "20240107";
-    release = fetchurl {
-      url = "https://github.com/xtaci/kcptun/releases/download/v${version}/kcptun-linux-amd64-${version}.tar.gz";
-      sha256 = "06mvky1a3is7fgh2q9fi8l4g17v02j54grm8zm3fms6h9xncw4ak";
+  inherit (builtins) fetchurl;
+  kcptunBinaries = {
+    "x86_64-linux" = rec {
+      version = "20240107";
+      release = fetchurl {
+        url = "https://github.com/xtaci/kcptun/releases/download/v${version}/kcptun-linux-amd64-${version}.tar.gz";
+        sha256 = "06mvky1a3is7fgh2q9fi8l4g17v02j54grm8zm3fms6h9xncw4ak";
+      };
+      client = "client_linux_amd64";
+      server = "server_linux_amd64";
     };
-    client = "client_linux_amd64";
-    server = "server_linux_amd64";
-  };
-  "aarch64-linux" = rec {
-    version = "20240107";
-    release = fetchurl {
-      url = "https://github.com/xtaci/kcptun/releases/download/v${version}/kcptun-linux-arm64-${version}.tar.gz";
-      sha256 = "137rsciif2vqix53bmxq1b67rn6bw2a1h7wvq8fl93hshdckp9rv";
+    "aarch64-linux" = rec {
+      version = "20240107";
+      release = fetchurl {
+        url = "https://github.com/xtaci/kcptun/releases/download/v${version}/kcptun-linux-arm64-${version}.tar.gz";
+        sha256 = "137rsciif2vqix53bmxq1b67rn6bw2a1h7wvq8fl93hshdckp9rv";
+      };
+      client = "client_linux_arm64";
+      server = "server_linux_arm64";
     };
-    client = "client_linux_arm64";
-    server = "server_linux_arm64";
   };
-};
-kcptunBinary = kcptunBinaries."${stdenvNoCC.hostPlatform.system}" or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
+  kcptunBinary = kcptunBinaries."${stdenvNoCC.hostPlatform.system}" or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
 in
 stdenvNoCC.mkDerivation {
   pname = "kcptun-bin";
