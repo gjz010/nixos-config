030e51e9db1b570c385eac3e16de371c3d4b6014
gjz010 <gjz010944@gmail.com>
2024-08-12 10:32:33 +0800

    Autocommit from gjz010-nixos-box on 2024年 08月 12日 星期一 10:32:33 CST

--- machines/gjz010-nixos-box/configuration.nix
+++ machines/gjz010-nixos-box/configuration.nix
@@ -53,7 +53,7 @@
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-
+  programs.hyprland.enable = true;
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
--- machines/gjz010-nixos-box/users/gjz010.nix
+++ machines/gjz010-nixos-box/users/gjz010.nix
@@ -9,9 +9,29 @@
     neededForUsers = true;
   };
   home-manager.users."gjz010" = { pkgs, ... }: {
-    home.packages = with pkgs; [ cowsay vscodium zotero_7 ripgrep gjz010.pkgs.proxychains-wrapper gjz010.pkgs.icalinguapp ];
+    home.packages = with pkgs; [ cowsay vscodium zotero_7 ripgrep gjz010.pkgs.proxychains-wrapper
+        gjz010.pkgs.icalinguapp kdePackages.spectacle
+        kdePackages.polkit-kde-agent-1 flameshot hyprpaper rofi-wayland
+        (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+        font-awesome grimblast hyprlock grim imagemagick pavucontrol qpwgraph
+    ];
     programs.bash.enable = true;
-
+    programs.waybar.enable = true;
+    services.dunst = {
+      enable = true;
+      settings = {
+        global = {
+          origin = "bottom-right";
+        };
+      };
+    };
+    fonts.fontconfig.enable = true;
+    home.pointerCursor = {
+      gtk.enable = true;
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Adwaita";
+      size = 16;
+    };
     # The state version is required and should stay at the version you
     # originally installed.
     home.stateVersion = config.system.stateVersion;
